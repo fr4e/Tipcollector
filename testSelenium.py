040c5ca75e6eb9a9995535eccc8f8b17a36e8b9a
fr4e <38686404+fr4e@users.noreply.github.com>
2018-10-03 17:49:50 +0900

    crawling
    Add function to crawling Tip table

--- testSelenium.py
+++ testSelenium.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
-def outputSource(text):
-    f = open("D:\\python\\test.html", 'w', encoding='UTF-8')
+def outputSource(text, filename):
+    f = open("D:\\python\\" + filename, 'w', encoding='UTF-8')
     try:
         f.write(text)
     except Exception as e:
@@ -15,7 +15,7 @@ def outputSource(text):
 
 
 
-def test():
+def createReportFile():
     #ブラウザを立ち上げずに操作するヘッドレスモードとやらがあるそうな
     #options = Options()
     #potions.add_argument("--headless")
@@ -26,11 +26,44 @@ def test():
     #browser.get("http://seleniumhp.org/")
     browser.get("https://datacentersupport.lenovo.com/jp/ja/solutions/ht507201")
     wait.until(expected_conditions.presence_of_element_located((By.ID, "contentBody")))
-    outputSource(browser.page_source)
+    outputSource(browser.page_source, "test.html")
+    
+
     #print(browser.find_element_by_tag_name("p"))
+
+def fetchList():
+    #ブラウザを立ち上げずに操作するヘッドレスモードとやらがあるそうな
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Firefox(firefox_options=options)
+
+    #browser = webdriver.Firefox()
+    wait = WebDriverWait(browser, 10)
+    browser.get("https://kimkaz@jp.ibm.com:Juloct10@servicetools.lenovo.com/latesttips")
+    wait.until(expected_conditions.presence_of_element_located((By.ID, "latestx")))
+    #outputSource(browser.page_source, "GLOSSE.html")
+    get_table_element = browser.find_element_by_tag_name("table")
+    get_row_elements = get_table_element.find_elements_by_tag_name("tr")
+    get_row_url = get_row_elements[2].find_elements_by_tag_name("a")
+
+    for i in range(1, len(get_row_elements)):
+        get_row_url = get_row_elements[i].find_elements_by_tag_name("a")
+        
+        print(get_row_elements[i].text)
+        if len(get_row_url) > 0:
+            print(get_row_url[1].get_attribute("href"))
+
+        print(" ")
+
+
+    
+    #
+    #print(get_row_elements[2].text)
+    #output URL
+    #print(get_row_url[1].get_attribute("href"))
 
 
 if __name__ == '__main__':
-    test()
+    fetchList()
     print("END")
     
